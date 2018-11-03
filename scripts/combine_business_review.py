692f031663d15c84584fa1277785eacffd6e3c58
zhuifengmu@gmail.com <kayleyang@Kayles-MacBook-Pro.local>
2018-11-03 16:09:02 -0400

    small refinement

--- scripts/combine_business_review.py
+++ scripts/combine_business_review.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 # load business data
-with open("data/yelp_academic_dataset_business.json", 'r') as business:
+with open("../data/yelp_academic_dataset_business.json", 'r') as business:
     business_jsons = [json.loads(line) for line in business.readlines()]
 
 # convert jsons to data
@@ -10,11 +10,11 @@ with open("data/yelp_academic_dataset_business.json", 'r') as business:
 business = pd.DataFrame(business_jsons)[['business_id', 'stars', 'name', 'categories', 'latitude', 'longitude', 'state', 'review_count']]
 
 # load review data
-reviews = pd.read_table('data/reviews_2017.tsv', sep='\t',
+reviews = pd.read_table('../data/reviews_2017.tsv', sep='\t',
                         names=['business_id', 'cool', 'date', 'funny', 'likes', 'review_id', 'review_stars', 'text', 'useful', 'user_id'])
 
 # join two datasets
-combined_ = business.set_index('business_id').join(reviews.set_index('business_id'), how='inner')
+combined_ = business.join(reviews,on='business_id', how='inner')
 
 # write to tsv
-combined_.to_csv('data/business_review_2017.tsv', sep='\t')
+combined_.to_csv('../data/business_review_2017.tsv', sep='\t')
